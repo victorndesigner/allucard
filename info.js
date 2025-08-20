b062f45460baa40c319d4c6c8f3e143ca5612fd2
bolttexturas <137934167+victorndesigner@users.noreply.github.com>
2025-08-19 22:59:58 -0300

    att

---
+++ info.js
@@ -0,0 +1,10 @@
+const servidorNome = "E_Dream-X0026";
+
+const blocosInfo = [
+  {
+    nome: "Nsfrt_Allucard",
+    id: "150681301",
+    mostrar: true,
+    imagem: "img/Nsfrt_Allucard.png"
+  },
+];
